ef2a16e5b9470a7b147f5c6267c7af5ac062ab23
Avery Aaron <sylvanserene@gmail.com>
2024-06-29 18:58:48 +0300

    Added references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 import { NavComponent } from "./nav/nav.component";
 import { DetailComponent } from "./detail/detail.component";
+import { ReferencesComponent } from "./references/references.component";
 import { MapComponent } from './map/map.component';
 
 @Component({
@@ -10,6 +11,6 @@ import { MapComponent } from './map/map.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [MapComponent, DetailComponent, NavComponent, RouterOutlet]
+    imports: [MapComponent, DetailComponent, NavComponent, ReferencesComponent, RouterOutlet]
 })
 export class AppComponent {}
